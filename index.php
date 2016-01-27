bdde3bef6f08246315d7c418e4a7b15c72870d68
Nico Watine <contact@studioncreations.com>
2016-01-27 00:17:59 -0600

    Initial commit of CABC

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Clements Athletic Booster Club | Clements High School</title>
+	<meta name="description" content="###">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="author" href="/humans.txt">
+
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
+		<script src="<?php autoVer('/js/ie-min.js'); ?>"></script>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/vendor/jquery/1-11-3/jquery-min.js"><\/script>')</script>
+<script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
+
+</body>
+</html>
