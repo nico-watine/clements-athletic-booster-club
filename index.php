f6e30bedc4670b8336df79ac737122bd091899c8
Nico Watine <nico@studioncreations.com>
2016-03-07 17:58:48 -0600

    Clean slate for index
    <footer> saved to _ignores file
    Same with facebook-script

--- index.php
+++ index.php
@@ -32,135 +32,23 @@
 	<![endif]-->
 </head>
 <body>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1674633382806636',
-      xfbml      : true,
-      version    : 'v2.5'
-    });
-  };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
 
-<div class="height-wrapper">
-	
-<header>
-	<div id="logo-shade"></div>
-	<address>4200 Elkins Rd | Sugar Land, TX 77479</address>
-	<div class="outer-container">
-		<div class="row">
-			<div class="medium-2 columns logo">
-				<a href="/"><img src="/img/assets/logo/logo.png" srcset="/img/assets/logo/logo@2x.png 2x" alt="Clements High School Logo"></a>
-			</div>
-			<div class="medium-10 columns" id="masthead">
-				<div id="site-name">
-					<h1 class="header">Clements Athletic <span>Booster Club</span></h1>
-					<h2>Clements High School <span>| <strong>FBISD</strong></span></h2>
-				</div>
-				<nav id="header-nav">
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Baseball</a></li>
-						<li><a href="#">Basketball</a></li>
-						<li><a href="#">Cheer</a></li>
-						<li><a href="#">Football</a></li>
-						<li><a href="#">Golf</a></li>
-						<li><a href="#">Soccer</a></li>
-						<li><a href="#">Swimming</a></li>
-						<li><a href="#">Tennis</a></li>
-						<li><a href="#">Track &amp; Field</a></li>
-						<!-- <li><a href="#">Calendar</a></li> -->
-						<!-- <li><a href="#">Join CABC</a></li> -->
-					</ul>
-				</nav>
-			</div>
-			<!-- <nav id="header-nav">
-				<ul>
-					<li><a href="#">Home</a></li>
-					<li><a href="#">Baseball</a></li>
-					<li><a href="#">Basketball</a></li>
-					<li><a href="#">Cheer</a></li>
-					<li><a href="#">Football</a></li>
-					<li><a href="#">Golf</a></li>
-					<li><a href="#">Soccer</a></li>
-					<li><a href="#">Swimming</a></li>
-					<li><a href="#">Tennis</a></li>
-					<li><a href="#">Track &amp; Field</a></li>
-					<li><a href="#">Calendar</a></li>
-					<li><a href="#">Join CABC</a></li>
-				</ul>
-			</nav> -->
-		</div>
-</header>
 
-<main style="text-align: center; margin-top: 100px;">
-	<h1 class="header">Full site on the way</h1>
-	<h3 style="margin-bottom: 30px;">Spread the word!</h3>
-	<div class="fb-like" data-href="http://clementsathleticboosterclub.org/" data-layout="box_count" data-action="like" data-show-faces="false" data-share="true"></div>
-</main>
-
-<footer>
-	<div class="row">
-		<div id="tricolor"><span id="white"></span><span id="light-blue"></span><span id="navy-blue"></span></div>
-		<div class="small-12 medium-6 columns site-nav">
-			<h4 class="header">CABC Sports</h4>
-			<nav id="footer-nav">
-				<ul id="column-1">
-					<li><a href="#">Home</a></li>
-					<li><a href="#">Baseball</a></li>
-					<li><a href="#">Basketball</a></li>
-					<li><a href="#">Cheer</a></li>
-					<li><a href="#">Football</a></li>
-				</ul>
-				<ul id="column-2">
-					<li><a href="#">Golf</a></li>
-					<li><a href="#">Soccer</a></li>
-					<li><a href="#">Swimming</a></li>
-					<li><a href="#">Tennis</a></li>
-					<li><a href="#">Track &amp; Field</a></li>
-					<!-- <li><a href="#">Calendar</a></li> -->
-					<!-- <li><a href="#">Join CABC</a></li> -->
-				</ul>
-			</nav>
-		</div>
-		<div class="small-12 medium-6 columns footer-info">
-			<div class="school-details">
-				<h4 class="header">Clements High School</h4>
-				<p>A Fort Bend ISD School</p>
-				<p><a href="https://goo.gl/maps/BP1Xx2EdqK22" target="_blank">4200 Elkins Rd | Sugar Land, TX 77479</a></p>
-			</div>
-			<div class="affiliate-sites">
-				<h4 class="header">Affiliate Sites</h4>
-				<p><a href="http://www.fortbendisd.com/chs" target="_blank">CHS Official Website</a></p>
-				<p><a href="http://www.fortbendisd.com/" target="_blank">FBISD Official Website</a></p>
-				<p id="colophon"><a href="http://studioncreations.com" target="_blank">A <span>STUDIO N</span> CREATION</a></p>
-			</div>
-		</div>
-	</div>
-</footer>
-</div>
 
 <script src="/bower_components/jquery/dist/jquery.js"></script>
 <script src="/bower_components/what-input/what-input.js"></script>
 <script src="/bower_components/foundation-sites/dist/foundation.js"></script>
 <script src="<?php autoVer('/js/min/app-min.js'); ?>"></script>
 
-<script>
+<!--<script>
 	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
 	function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
 	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
 	e.src='https://www.google-analytics.com/analytics.js';
 	r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
 	ga('create','UA-55459011-9','auto');ga('send','pageview');
-</script>
+</script>-->
 
 </body>
 </html>
