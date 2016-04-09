14e44cc7c9d420f08fe04e29dc04235e715d3a94
Nico Watine <nico@studioncreations.com>
2016-04-09 15:06:38 -0500

    site structure now fully imported into /dist
    Now figure out why certain styles are missing

---
+++ src/pages/cabc-golf-tournament-2016.php
@@ -0,0 +1,249 @@
+
+<header>
+	<address>4200 Elkins Rd | Sugar Land, TX 77479</address>
+	<div id="masthead">
+		<div class="row">
+			<div id="site-logo" class="medium-3 large-2 columns">
+				<div class="vertical-wrapper text-center">
+					<a href="/"><img srcset="http://cdn.studionclient.rocks/project/cabc/img/assets/logo/logo@2x.png 2x" src="http://cdn.studionclient.rocks/project/cabc/img/assets/logo/logo.png" alt="Clements High School Logo"></a>
+				</div>
+			</div>
+			<div id="site-name" class="medium-9 large-10 columns text-center medium-text-left">
+				<div class="vertical-wrapper">
+					<h1 class="title">Clements Athletic Booster Club</h1>
+					<h2>Clements High School | <strong>FBISD</strong></h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<nav>
+	<div class="row">
+		<ul class="menu">
+			<li><a href="/">Home</a></li>
+			<li class="active"><a href="/cabc-golf-tournament-2016">2016 Golf Tournament</a></li>
+			<li id="last"><a href="/cabc-sports">CABC Sports</a></li>
+			<!-- <li><a href="#">Join/Contact CABC</a></li> -->
+		</ul>
+	</div>
+</nav>
+<main>
+	<span itemscope itemtype="http://schema.org/Event">
+		<section class="row text-center" id="tournament-hero">
+			<h2 itemprop="name" id="banner-header" class="title"><span id="annual">5th Annual</span><span id="name">“Rangers On The Range”</span><span id="tournament">Golf Tournament</span></h2>
+			<h3 id="benefiting"><em>Benefiting the Clements Athletic Booster Club (CABC)</em></h3>
+		</section>
+		<div class="row" id="tournament-tag">
+			<p class="large-10 large-offset-1 columns p-x">The tournament, open to the public, will include 18 holes of golf, cart, lunch and dinner. Following the tournament, awards will be given.</p>
+		</div>
+		<section class="row text-center" id="tournament-deets">
+			<div class="medium-4 columns">
+				<div class="content-box" id="when">
+					<meta itemprop="startDate" content="2016-05-02">
+					<div class="prose">
+						<h3 class="title">When</h3>
+						<p>Monday <br>May 2, 2016</p>
+						<p>11AM Shotgun Start</p>
+					</div>
+					<div class="icon-bg"></div>
+				</div>
+			</div>
+			<div class="medium-4 columns">
+				<div class="content-box" id="where" itemprop="location" itemscope itemtype="http://schema.org/Place">
+					<div class="prose">
+						<h3 class="title">Where</h3>
+						<p itemprop="name">Sweetwater Country Club</p>
+						<p itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"><span itemprop="streetAddress">4400 Palm Royale Blvd</span> <br><span itemprop="addressLocality">Sugar Land</span>, <span itemprop="addressRegion">TX</span>  <span itemprop="postalCode">77479</span></p>
+					</div>
+					<div class="icon-bg"></div>
+				</div>
+			</div>
+			<div class="medium-4 columns">
+				<div class="content-box" id="cost" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+					<div class="prose">
+						<h3 class="title">Cost</h3>
+						<meta itemprop="priceCurrency" content="USD">
+						<p itemprop="price" content="150.00">$150 / Solo Player</p>
+						<p itemprop="price" content="500.00">$500/ Team of 4</p>
+					</div>
+					<div class="icon-bg"></div>
+				</div>
+			</div>
+		</section>
+	</span>
+	<section class="row" id="testimonial">
+		<div class="medium-3 columns" id="cabc-badge">
+			<img srcset="http://cdn.studionclient.rocks/project/cabc/img/events/golf-tournament/tournament-page/cabc-badge@2x.png 2x" src="http://cdn.studionclient.rocks/project/cabc/img/events/golf-tournament/tournament-page/cabc-badge.png" alt="">
+		</div>
+		<div class="medium-9 columns" id="quote">
+			<div class="wrapper">
+				<blockquote>
+					"We really want our community friends, alumni, and supporters of Clements athletics to come out and play. Fort Bend Independent School District does not provide all the necessary funding for sports, so our golf tournament is our biggest and most important fundraiser. It’s always a great time.”
+					<cite>Mitch Danklef | CABC President</cite>
+				</blockquote>
+			</div>
+		</div>
+	</section>
+	<section class="row p-x" id="sponsorship-section">
+		<div class="section-intro text-center">
+			<h2 class="title">Sponsorship Opportunities</h2>
+			<p>Expected Number of Players: Full field of 128 (1st signed up, 1st served), on the Pecan Course</p>
+		</div>
+		<div class="styled-content-container">
+			<ul class="tabs" data-tabs id="sponsorship-tabs">
+				<li class="tabs-title is-active"><a class="title" href="#title-sponsorship" aria-selected="true">Title</a></li>
+				<li class="tabs-title"><a class="title" href="#hole-in-one">Hole-In-One</a></li>
+				<li class="tabs-title"><a class="title" href="#lunch-dinner">Lunch/Dinner</a></li>
+				<li class="tabs-title"><a class="title" href="#hole-19">19th Hole</a></li>
+				<li class="tabs-title"><a class="title" href="#closest-to-pin">Closest To The Pin</a></li>
+				<li class="tabs-title"><a class="title" href="#longest-drive">Longest Drive</a></li>
+				<li class="tabs-title"><a class="title" href="#valet">Valet</a></li>
+				<li class="tabs-title"><a class="title" href="#hole">Hole</a></li>
+			</ul>
+			<div class="tabs-content" data-tabs-content="sponsorship-tabs">
+				<div class="tabs-panel is-active" id="title-sponsorship">
+					<h3 class="title sponsorship-name">Title Sponsorship</h3>
+					<h3 class="sponsorship-value">$10,000</h3>
+					<p>A unique opportunity for a corporate partner to be the exclusive title sponsor of this event and receive:</p>
+					<ul>
+						<li>Golfing for three (3) VIP foursomes, including VIP parking</li>
+						<li>Top billing of company name/logo on tournament signage at registration, putting greens and driving ranges</li>
+						<li>Company logo and tag line on the CABC website, including hyperlink to company’s web page</li>
+						<li>Two individual weeks’ (one per semester) use of CHS marquee, facing Sweetwater (at Elkins Rd) to display company message</li>
+						<li>Twelve (12) exclusive tweets from Clements High School’s twitter account on dates of your choice (4k followers)</li>
+						<li>Seven (7) dedicated email blasts, sent to all students, parents, teachers and alumni of CHS.</li>
+						<li>Full Page ad with forward position, in one season’s athletic program of your choice (2016-17 school year).</li>
+					</ul>
+				</div>
+				<div class="tabs-panel" id="hole-in-one">
+					<h3 class="title sponsorship-name">Hole-In-One Sponsorship</h3>
+					<h3 class="sponsorship-value">$5,000</h3>
+					<ul>
+						<li>Golfing for two (2) VIP foursomes, including VIP parking</li>
+						<li>Name/logo displayed on tournament signage on the putting greens and driving ranges</li>
+						<li>One week use of the Clements High School marquee, facing Sweetwater (at Elkins Rd) to display company message</li>
+						<li>Six (6) exclusive tweet from Clements High School’s twitter account on dates of your choice (4,000 followers)</li>
+						<li>Four (4) dedicated email blast, sent to all students, parents, teachers and alumni of CHS </li>
+						<li>One Page ad in one season’s athletic program of your choice (2016-17 school year)</li>
+					</ul>
+				</div>
+				<div class="tabs-panel" id="lunch-dinner">
+					<h3 class="title sponsorship-name">Lunch or Dinner Sponsorship</h3>
+					<h3 class="sponsorship-value">$3,500</h3>
+					<p>Underwrite lunch for an estimated 150 golfers and volunteers, and receive:</p>
+					<ul>
+						<li>Golfing for one (1) VIP foursome, including VIP parking</li>
+						<li>Signage at the lunch serving location & name/logo displayed on the putting greens and driving ranges. </li>
+						<li>Three days use of the CHS marquee, facing Sweetwater (at Elkins Rd) to display company message</li>
+						<li>Three (3) exclusive tweet from Clements High School’s twitter account (4,000 followers)</li>
+						<li>Two (2) dedicated email blast, sent to all students, parents, teachers and alumni of CHS </li>
+						<li>Half Page ad in one season’s athletic program of your choice (2016-17 school year)</li>
+					</ul>
+				</div>
+				<div class="tabs-panel" id="hole-19">
+					<h3 class="title sponsorship-name">19th Hole Sponsorship</h3>
+					<h3 class="sponsorship-value">$2,500</h3>
+					<p>Provide refreshments for the day at both the turn and 19th hole, and receive:</p>
+					<ul>
+						<li>Golfing for one (1) VIP foursome, including VIP parking</li>
+						<li>Name/logo exclusive signage at the turn, plus name/logo on signage at putting greens and driving ranges.</li>
+						<li>Two (2) exclusive tweets from Clements High School’s twitter account (4,000 followers)</li>
+						<li>One (1) dedicated email blast, sent to all students, parents, teachers and alumni of CHS </li>
+					</ul>
+				</div>
+				<div class="tabs-panel" id="closest-to-pin">
+					<h3 class="title sponsorship-name">'Closest To The Pin' Sponsorship (2 Available)</h3>
+					<h3 class="sponsorship-value">$1,500</h3>
+					<p>Two sponsors will underwrite the Closest to the Pin prize packages for their respective holes and will each receive:</p>
+					<ul>
+						<li>Golfing for one (1) VIP foursome, including VIP parking</li>
+						<li>Name/logo on hole signage on each par three on the course</li>
+						<li>One (1) exclusive tweet from Clements High School’s twitter account (4,000 followers)</li>
+					</ul>
+				</div>
+				<div class="tabs-panel" id="longest-drive">
+					<h3 class="title sponsorship-name">Longest Drive Sponsorship (2 Available)</h3>
+					<h3 class="sponsorship-value">$1,500</h3>
+					<p>Two sponsors will underwrite the Longest Drive prize packages for their respective holes and will each receive:</p>
+					<ul>
+						<li>Golfing for one (1) VIP foursome, including VIP parking</li>
+						<li>Name/logo on hole signage on each par five on the course</li>
+						<li>One (1) exclusive tweet from Clements High School’s twitter account (4,000 followers)</li>
+					</ul>
+				</div>
+				<div class="tabs-panel" id="valet">
+					<h3 class="title sponsorship-name">Valet Sponsorship</h3>
+					<h3 class="sponsorship-value">$1,500</h3>
+					<p>Underwrite the valet service for the tournament and receive:</p>
+					<ul>
+						<li>Golfing for one (1) VIP foursome, including VIP parking</li>
+						<li>Name/logo on exclusive signage at the valet stand at the entrance to Sweetwater Country Club</li>
+						<li>One (1) exclusive tweet from Clements High School’s twitter account (4,000 followers)</li>
+					</ul>
+				</div>
+				<div class="tabs-panel" id="hole">
+					<h3 class="title sponsorship-name">Hole Sponsorship</h3>
+					<h3 class="sponsorship-value">$750</h3>
+					<p>Sponsor one hole on the course and receive:</p>
+					<ul>
+						<li>Golf for one (1) VIP foursome, including VIP parking</li>
+						<li>Name/logo on signage at one hole</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="text-center" id="signup-contact">
+		<div class="blue-banner">
+			<h2 id="c2a" class="title">Get Signed Up!</h2>
+		</div>
+		<div class="row">
+			<!-- <h2 id="c2a" class="title">Sign Up Today!</h2> -->
+			<p id="appeal" class="medium-10 medium-offset-1 columns end padding">Our student athletes are the best in the district! Let’s show them how much we care. Sign up today to play or sponsor!</p>
+			<p id="info" class="small-12 columns">
+				<span>To sign up, please contact :</span>
+				<span id="name">Luis Gutierrez</span>
+				<span id="position">CABC Golf Tournament Chair</span>
+			</p>
+			<p id="phone" class="small-12 columns"><i class="icon-mobile-alt"></i>&nbsp;(832) 419-4717</p>
+			<p id="email" class="small-12 columns"><i class="icon-mail"></i>&nbsp;luis.m.gutierrez<span>@morganstanley.com</span></p>
+		</div>
+	</section>
+</main>
+
+<div id="logo-shade"><!-- opaque BG logo --></div>
+
+<footer>
+	<div class="row">
+		<div class="medium-6 columns text-center medium-text-left text-wrapper">
+			<h5 class="title">Clements High School</h5>
+			<p>A Fort Bend ISD School</p>
+			<a href="https://goo.gl/maps/jT3jsuKtRiB2" target="_blank">4200 Elkins Rd | Sugar Land, TX 77479</a>
+		</div>
+		<div class="medium-6 columns text-center medium-text-right text-wrapper">
+			<h5 class="title">Affiliate Sites</h5>
+			<a href="http://www.fortbendisd.com/chs" target="_blank">CHS Official Website &gt;&gt;</a><br><a href="http://www.fortbendisd.com/" target="_blank">FBISD Official Website &gt;&gt;</a>
+		</div>
+	</div>
+	<div class="row">
+		<div id="site-colophon" class="small-12 columns text-center">
+			<a href="http://studioncreations.com/" target="_blank">A <span>STUDIO N</span> CREATION</a>
+		</div>
+	</div>
+</footer>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="http://cdn.studionclient.rocks/project/cabc<?php autoVer('/js/tournament-tabs-min.js'); ?>"></script>
+<script src="http://cdn.studionclient.rocks/project/cabc/js/what-input-min.js"></script>
+
+
+<script>
+	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+	function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+	e.src='https://www.google-analytics.com/analytics.js';
+	r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+	ga('create','UA-55459011-9','auto');ga('send','pageview');
+</script>
+
+
